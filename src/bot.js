903d70f29d486054454039beed42a46495b9b01f
Jorge Sanchez <16sancjo@gmail.com>
2020-12-07 21:24:56 -0600

    leaderboard fix

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,8 @@
-require ('dotenv').config();
+require('dotenv').config();
 
-const {Client, MessageEmbed} = require('discord.js'); // Import client from discord.js
+const { Client, MessageEmbed } = require('discord.js'); // Import client from discord.js
 const client = new Client({             // Start instance of client
-    partials:['MESSAGE', 'REACTION']
+    partials: ['MESSAGE', 'REACTION']
 });
 const PREFIX = "&";                     // Prefix for the bot to use
 const Gear = require("../models/gear.js");  // Schema used for database
@@ -21,9 +21,9 @@ client.on('message', async (message) => {
     if (message.content.startsWith(PREFIX)) {   // If message starts with specified prefix
         // Trims off the prefix and splits message into command (CMD_NAME) and an array of the passed args
         const [CMD_NAME, ...args] = message.content
-        .trim()
-        .substring(PREFIX.length)
-        .split(/\s+/);  // Regex to eliminate white spaces
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);  // Regex to eliminate white spaces
 
         if (CMD_NAME === 'kick') {                      // Kick command
             if (!message.member.hasPermission('KICK_MEMBERS'))
@@ -33,8 +33,8 @@ client.on('message', async (message) => {
             const member = message.guild.members.cache.get(args[0]);
             if (member) {
                 member.kick()
-                .then((member) => message.channel.send(`${member} was kicked.`))
-                .catch((err) => message.channel.send('I cannot kick that user :('))
+                    .then((member) => message.channel.send(`${member} was kicked.`))
+                    .catch((err) => message.channel.send('I cannot kick that user :('))
             } else {
                 message.channel.send('That member was not found');
             }
@@ -50,7 +50,7 @@ client.on('message', async (message) => {
             } catch (error) {
                 message.channel.send('An error occured. Either I do not have permissions or the user was not found.');
             }
-        
+
         } else if (CMD_NAME === 'gear') {                   // Gearbot command
             if (args.length === 0)      // If there are no arguments, requests the user for input
                 return message.reply('please provide either a link to your gear or a user ID.');
@@ -64,53 +64,53 @@ client.on('message', async (message) => {
                         message.reply(`that user is not in the database.`);
                     } else {
                         message.channel.send(new MessageEmbed()
-                        .setColor('#07772B')
-                        .setTitle(member.username)
-                        .setDescription('AP: ' + gear.ap + '\nAAP: ' + gear.aap + '\nDP: ' + gear.dp)
-                        .setThumbnail('https://cdn.discordapp.com/attachments/742673775853830245/769642313449209867/IngenMix1.png')
-                        .setImage(gear.gearLink));
+                            .setColor('#07772B')
+                            .setTitle(member.username)
+                            .setDescription('AP: ' + gear.ap + '\nAAP: ' + gear.aap + '\nDP: ' + gear.dp)
+                            .setThumbnail('https://cdn.discordapp.com/attachments/742673775853830245/769642313449209867/IngenMix1.png')
+                            .setImage(gear.gearLink));
                     }
                 });
 
             } else {    // Else if the message doesn't @ a user
-            const filter = message.author.id;
-            const update = args[0];
-            if (validURL(args[0]) === true) {
-                Gear.findOneAndUpdate({ userID: filter }, {$set:{ gearLink: update }}, {new: true}, (err, gear) => {
-                    if (err) console.log("Something wrong with updating data");
-                    if (!gear) {
-                        const gear = new Gear({
-                        userID: message.author.id,
-                        gearLink: args[0]
-                        });
-                        gear.save().catch(err => console.log(err)).then(message.channel.send('You have been added to the database!'));
-                    }
-                });
-                message.channel.send('your gear has been updated!');
-            } else {
-                message.reply('invalid URL.');
+                const filter = message.author.id;
+                const update = args[0];
+                if (validURL(args[0]) === true) {
+                    Gear.findOneAndUpdate({ userID: filter }, { $set: { gearLink: update } }, { new: true }, (err, gear) => {
+                        if (err) console.log("Something wrong with updating data");
+                        if (!gear) {
+                            const gear = new Gear({
+                                userID: message.author.id,
+                                gearLink: args[0]
+                            });
+                            gear.save().catch(err => console.log(err)).then(message.channel.send('You have been added to the database!'));
+                        }
+                    });
+                    message.channel.send('your gear has been updated!');
+                } else {
+                    message.reply('invalid URL.');
+                }
             }
-        }
 
         } else if (CMD_NAME === 'input') {                  // Input command
             const user = message.author.id;
             for (i = 0; i < args.length; i++) {
-                const update = args[i+1];
+                const update = args[i + 1];
 
                 if (args[i] === 'ap') {
-                    Gear.findOneAndUpdate({ userID: user }, { $set: { ap: update }}, {new: true}, (err, gear) => {
+                    Gear.findOneAndUpdate({ userID: user }, { $set: { ap: update } }, { new: true }, (err, gear) => {
                         if (err) message.channel.send('Error updating your AP, try $help for more info.');
                         else message.channel.send('Your AP has been updated!');
                     });
                     i++;
                 } else if (args[i] === 'aap') {
-                    Gear.findOneAndUpdate({ userID: user }, { $set: { aap: update }}, {new: true}, (err, gear) => {
+                    Gear.findOneAndUpdate({ userID: user }, { $set: { aap: update } }, { new: true }, (err, gear) => {
                         if (err) message.channel.send('Error updating your Awakening AP, try $help for more info.')
                         else message.channel.send('Your Awakening AP has been updated!');
                     });
                     i++;
                 } else if (args[i] === 'dp') {
-                    Gear.findOneAndUpdate({ userID: user }, { $set: { dp: update }}, {new: true}, (err, gear) => {
+                    Gear.findOneAndUpdate({ userID: user }, { $set: { dp: update } }, { new: true }, (err, gear) => {
                         if (err) message.channel.send('Error updating your DP, try $help for more info.');
                         else message.channel.send('Your DP has been updated!');
                     });
@@ -120,50 +120,54 @@ client.on('message', async (message) => {
 
         } else if (CMD_NAME === 'leaderboard') {                    // Leaderboard command
             /* add multiple leaderboard top 10, 15, 20, 50 */
+            // ** GS is sorted properly, but stats and usernames are not properly tied ** //
             Gear.aggregate([
-                { $group: { _id: '$userID', gs:  { $sum: { $add: [{ $divide: [{$add: ['$ap', '$aap']}, 2] }, '$dp'] } } } }, // Group by userID and calculates gearscore ((ap+aap/2)+dp)
+                {
+                    $project: {
+                        _id: '$userID',
+                        ap: 1,
+                        aap: 1,
+                        dp: 1,
+                        gs: { $sum: { $add: [{ $divide: [{ $add: ['$ap', '$aap'] }, 2] }, '$dp'] } },
+                    }
+                }, // Group by userID and calculates gearscore ((ap+aap/2)+dp)
                 { $sort: { gs: -1 } },  // Sorts the group by ascending gearscore
                 { $limit: 10 }          // limits the output to top 10 only
             ]).exec((err, res) => {
                 if (err) console.log(err);
-                // I have no idea what I did here
-                Gear.find().exec((err, ress) => {
-                    if (err) console.log(err);
-                    let embed = new MessageEmbed().setTitle("Leaderboard").setThumbnail('https://cdn.discordapp.com/attachments/742673775853830245/769642313449209867/IngenMix1.png');
-
-                    if (ress.length === 0)
-                        embed.setColor("RED").addField("No data found.");
-                    embed.setColor("#07772B");
-                    for (i = 0; i < ress.length; i++) {
-                        let member = message.guild.members.cache.get(ress[i].userID) || "User Left"     // Add family name field as solution -!
-                        message.guild.members.fetch(ress[i].userID);
-                        if (member === "User Left") {
-                            embed.addField(`${i+1}. ${member}`, `**AP:** ${ress[i].ap}` + ` **AAP:** ${ress[i].aap}` + ` **DP:** ${ress[i].dp}` + ` **GS:** ${Math.round(res[i].gs)}`);
-                        } else {
-                            embed.addField(`${i+1}. ${member.user.username}`, `**AP:** ${ress[i].ap}` + ` **AAP:** ${ress[i].aap}` + ` **DP:** ${ress[i].dp}` + ` **GS:** ${Math.round(res[i].gs)}`);
-                        }
+                let embed = new MessageEmbed().setTitle("Leaderboard").setThumbnail('https://cdn.discordapp.com/attachments/742673775853830245/769642313449209867/IngenMix1.png');
+                if (res.length === 0)
+                    embed.setColor("RED").addField("No data found.");
+                embed.setColor("#07772B");
+                for (i = 0; i < res.length; i++) {
+                    let member = message.guild.members.cache.get(res[i].userID) || "User Left"     // Add family name field as solution -!
+                    message.guild.members.fetch(res[i].userID);
+                    if (member === "User Left") {
+                        embed.addField(`${i + 1}. ${member}`, `**AP:** ${res[i].ap}` + ` **AAP:** ${res[i].aap}` + ` **DP:** ${res[i].dp}` + ` **GS:** ${Math.round(res[i].gs)}`);
+                    } else {
+                        embed.addField(`${i + 1}. ${member.user.username}`, `**AP:** ${res[i].ap}` + ` **AAP:** ${res[i].aap}` + ` **DP:** ${res[i].dp}` + ` **GS:** ${Math.round(res[i].gs)}`);
                     }
-                    message.channel.send(embed);
-                })
+                }
+                message.channel.send(embed);
             })
         } else if (CMD_NAME === 'help') {
             let embed = new MessageEmbed()
-            .setTitle('Help Center')
-            .setThumbnail('https://cdn.discordapp.com/attachments/742673775853830245/769642313449209867/IngenMix1.png')
-            .setColor('07772B')
-            .addField('$gear', 'To look up a users gear provide @user, to update your gear provide a valid link to your gear image.')
-            .addField('$leaderboard', 'Displays the top 25 players in the guild sorted by gearscore. If you do not use $input you will not show up on the leaderboard.')
-            .addField('$input', 'Manual input for your stats. use $input <stat> <number> to update. Example: $input ap 162. You can update more than one stat at a time. Example: $input ap 162 aap 203 dp 301');
+                .setTitle('Help Center')
+                .setThumbnail('https://cdn.discordapp.com/attachments/742673775853830245/769642313449209867/IngenMix1.png')
+                .setColor('07772B')
+                .addField('$gear', 'To look up a users gear provide @user, to update your gear provide a valid link to your gear image.')
+                .addField('$leaderboard', 'Displays the top 25 players in the guild sorted by gearscore. If you do not use $input you will not show up on the leaderboard.')
+                .addField('$input', 'Manual input for your stats. use $input <stat> <number> to update. Example: $input ap 162. You can update more than one stat at a time. Example: $input ap 162 aap 203 dp 301');
 
             message.channel.send(embed);
-        }else {
+        } else {
             message.reply('Invalid command.');
         }
     }
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
-    const {name} = reaction.emoji;
+    const { name } = reaction.emoji;
     const member = reaction.message.guild.members.cache.get(user.id)
     if (reaction.message.id === '752459416506204211') {
         switch (name) {
@@ -184,7 +188,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 });
 
 client.on('messageReactionRemove', (reaction, user) => {
-    const {name} = reaction.emoji;
+    const { name } = reaction.emoji;
     const member = reaction.message.guild.members.cache.get(user.id)
     if (reaction.message.id === '752459416506204211') {
         switch (name) {
@@ -205,13 +209,13 @@ client.on('messageReactionRemove', (reaction, user) => {
 });
 
 function validURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  return !!pattern.test(str);
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
 }
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
